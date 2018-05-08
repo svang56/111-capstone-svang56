60e95f9ffcea05f9dfe983160411b83c34c59c30
svang56 <svang56@mail.csuchico.edu>
2018-05-07 18:45:37 -0700

    asfd

---
+++ castle.cpp
@@ -0,0 +1,25 @@
+//Author: Sunny Vang
+#include<string>
+//this will be the base class for the 3 fighting areas / will make own 
+//class for the town itself
+
+class fighting
+{
+    private:
+        std::string blackKnight;
+        std::string guard
+        std::;
+        
+    public:
+        void encounterMonster();//this will randomly 
+        bool fightOrRun(int);//if 1 then fight, if 2 then run
+        void generateMonster(string, string, string);//th
+        void randomChance();//this will determine win chance for monster
+        void stayOrGo();//asks if want to fight again or leave
+        //to get monster names, initialize within the do-while loop 
+        //in the int main() into the constructor 
+
+        //Each class inheriting will have their own constructor
+        //and will have private variables of each monster
+
+}
