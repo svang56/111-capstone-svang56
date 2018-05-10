b6e6568d2633ddfa23f68758c9cd8f6e20323d37
svang56 <svang56@mail.csuchico.edu>
2018-05-10 16:22:28 -0700

    Working

---
+++ town.h
@@ -0,0 +1,17 @@
+//Author: Sunny Vang    
+#ifndef TOWN_H_
+#define TOWN_H_
+
+#include<iostream>
+#include<string>
+
+class Town: public Adventure
+{
+
+    public:
+       void shopping();
+       void goToInn();
+};
+
+#endif//town.h
+
