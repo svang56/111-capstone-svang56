3b902b1b7fc80585bfb9df3467273da095c58552
svang56 <svang56@mail.csuchico.edu>
2018-05-10 12:00:13 -0700

    Send

--- fighting.h
+++ fighting.h
@@ -13,14 +13,17 @@ class fighting
         std::string bossMonster;
         
     public:
-        void getMonsters(std::string, std::string, std::string)
+        void getMonsters(std::string, std::string, std::string)//this will get monster
+        //for the area
+
+        std::string randomMonster();
         
         void encounterMonster(std::string);//this will randomly spawn monster for player to fight
         bool fightOrRun(std::string);//if 1 then fight, if 2 then run`
         void generateMonster(std::string, std::string, std::string);
         //this will generate monsters 
 
-        bool chanceBattle(std::string);//this will determine win chance for monster
+        bool battleChance(std::string);//this will determine win chance for monster
         //if monster == this, then that chance to win
 
         std::string stayOrGo();//asks if want to fight again or leave or use potion
